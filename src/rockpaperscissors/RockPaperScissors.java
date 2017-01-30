63712680faec0f131d8d542fce0a63083e81c1b5
Cameryn Holstick <cholstick0104@gmail.com>
2017-01-30 15:56:42 -0500

    Delete nested switch

--- src/rockpaperscissors/RockPaperScissors.java
+++ src/rockpaperscissors/RockPaperScissors.java
@@ -25,68 +25,31 @@ public class RockPaperScissors {
                            + "Please enter your choice of rock, paper, or scissors..");
         
         String input = (new Scanner(System.in)).next();
-        
         usrHand = new Player(input);
-        /*if (input.equalsIgnoreCase("rock")) 
-        {
-            usrHand = new Player(1);
-        } else if (input.equalsIgnoreCase("paper")) 
-        {
-            usrHand = new Player(2);
-        } else if (input.equalsIgnoreCase("scissors")) 
-        {
-            usrHand = new Player(3);
-        }
-        else usrHand = new Player(); */
-
         compHand = new Player(ThreadLocalRandom.current().nextInt(1, 3 + 1));
-      
         
         System.out.println("User: " + usrHand.getHand() + " Vs. Computer: " + compHand.getHand());
         
-        /*The following switch statements will decide the winner of the game.
+        /*The following switch statement will decide the winner of the game.
           1=rock; 2=paper; 3=scissors
           1>3; 1<2; 2<3;*/
         switch(usrHand.getNumValue())
         {
             case 1:
-                
-                switch(compHand.getNumValue())
-                { 
-                    case 1:
-                        System.out.println("Tie");
-                    case 2:
-                        System.out.println("Computer Won");
-                    case 3:
-                        System.out.println("You Win!");
-                    break;
-                }
-                
+                if(compHand.getNumValue() == 1) System.out.println("Tie");
+                else if(compHand.getNumValue() == 2) System.out.println("Computer Won");
+                else if(compHand.getNumValue() == 3) System.out.println("You Win!");
+                break;
             case 2:
-                
-                switch(compHand.getNumValue()) 
-                {
-                    case 1:
-                        System.out.println("You Win!");
-                    case 2:
-                        System.out.println("Tie");
-                    case 3:
-                        System.out.println("Computer Won");
-                    break;
-                }
-                
+                if(compHand.getNumValue() == 1) System.out.println("You Win!");
+                else if(compHand.getNumValue() == 2) System.out.println("Tie");
+                else if(compHand.getNumValue() == 3) System.out.println("Computer Won");
+                break;
             case 3:
-                
-                switch(compHand.getNumValue())
-                {
-                    case 1:
-                        System.out.println("Computer Won");
-                    case 2:
-                        System.out.println("You Win!");
-                    case 3:
-                        System.out.println("Tie");
-                    break;
-                }
+                if(compHand.getNumValue() == 1) System.out.println("Computer Won");
+                else if(compHand.getNumValue() == 2) System.out.println("You Win!");
+                else if(compHand.getNumValue() == 3) System.out.println("Tie");
+                break;
         }
         
     }
